d88f7c3536796df7b71055f6d69218dd0e42496e
Guiherme Oliveira <81393701+gui013memo@users.noreply.github.com>
2022-04-24 22:30:54 -0300

    Add files via upload

---
+++ correto-1.ps1
@@ -0,0 +1,10 @@
+﻿Send-MailMessage 
+-From guilhermeadm016@gmail.com
+-Subject "teste envio auto" 
+-To guilhermeadm016@gmail.com 
+-Attachments C:\temp\testeAttach.txt 
+-Body "teste envio autoteste envio autoteste envio auto" 
+-Credential guilhermeadm016@gmail.com 
+-Port 587 
+-SmtpServer smtp.gmail.com 
+-UseSsl
---
+++ correto-22.ps1
@@ -0,0 +1,7 @@
+﻿$userName = 'guilhermeadm016@gmail.com'
+$password = 'ffodase123'
+[SecureString]$securepassword = $password | ConvertTo-SecureString -AsPlainText -Force 
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $securepassword
+Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From guilhermeadm016@gmail.com -To guilhermeadm016@gmail.com -Subject 'Test subject' -Attachments C:\temp\testeAttach.txt -Body 'Test message' -Credential $credential
+write-host "Email enviado com sucesso!" -ForegroundColor black -backgroundcolor white
+pause
